301cc4c39fc9a3fa40e6abe45850adc66af5e16e
UddhavKhekade <uddhavkhekade@gmail.com>
2026-08-21 20:57:37 +0530

    api of article and blog generation integrated

--- Server/controllers/aicontrollers.js
+++ Server/controllers/aicontrollers.js
@@ -1,6 +1,9 @@
 
 import { OpenAI } from "openai/client.js"
+// import { GoogleGenAI } from "@google/genai";
 import sql from "../configs/db.js"
+import {v2 as cloudinary} from 'cloudinary'
+import axios from 'axios'
 
 const AI = new OpenAI({
     apiKey : process.env.GEMINI_API_KEY,
@@ -9,7 +12,7 @@ const AI = new OpenAI({
 
 
 
-export const generateArticle = async ()=>{
+export const generateArticle = async (req,res)=>{
     try{
         const {userId} = req.auth()
         const {prompt , length}= req.body
@@ -18,17 +21,16 @@ export const generateArticle = async ()=>{
 
 
         const response = await AI.chat.completions.create({
-            model : "gemini-2.0-flash",
-            messages :[{
-                role : "user",
-                content : prompt,
-            },
+            model : "gemini-3.7-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
             ],
-            temperature : 0.7,
-            max_tokens : length
         });
 
-        const content = response.choices[0].message.content
+        const content = response.choices[0].message.content 
 
         await sql`INSERT INTO creations (user_id,prompt,content,type) 
         VALUES(${userId},${prompt},${content},'article')`;
@@ -39,6 +41,91 @@ export const generateArticle = async ()=>{
     }catch(error){
         console.log(error.message)
         res.json({success : false ,message : error.message })
+
+    }
+}
+
+
+export const generateBlogTitle = async (req,res)=>{
+    try{
+        const {userId} = req.auth()
+        const {prompt }= req.body
+        const plan = req.plan
+        const free_usage = req.free_usage
+
+
+        const response = await AI.chat.completions.create({
+            model : "gemini-3.7-flash",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+
+        const content = response.choices[0].message.content 
+
+        await sql`INSERT INTO creations (user_id,prompt,content,type) 
+        VALUES(${userId},${prompt},${content},'BlogTitle')`;
+
+
+        res.json({success:true , content})
+        
+    }catch(error){
+        console.log(error.message)
+        res.json({success : false ,message : error.message })
+
+    }
+}
+
+export const generateImage = async (req,res)=>{
+    try{
+        const {userId} = req.auth()
+        const {prompt ,publish}= req.body
+        const plan = req.plan
+       
+
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+
+        
+        const {data} = await axios.post("https://clipdrop-api.co/text-to-image/v1",formData,{
+          
+            headers: {
+            'x-api-key': process.env.CLIPDROP_API_KEY,
+            },
+            responseType : "arraybuffer",
+        })
+
+        const base64Image = `data:image/png;base64,${Buffer.from(data).
+            toString('base64Image')
+        }`
+        
+        const {secure_url} = await cloudinary.uploader.upload(base64Image)
+
+        await sql`INSERT INTO creations (user_id,prompt,content,type,publish) 
+        VALUES(${userId},${prompt},${secure_url},'Image',${publish ?? false})`;
+
+
+        res.json({success:true ,content: secure_url})
+        
+    }catch(error){
+       console.log("STATUS:", error.response?.status);
+
+        if (error.response?.data) {
+            console.log(
+                "DATA:",
+                Buffer.from(error.response.data).toString("utf8")
+            );
+        }
+
+        console.log("MESSAGE:", error.message);
+
+        res.json({
+            success: false,
+            message: error.message
+        });
 
     }
 }
--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from './routes/airoutes.js'
+import connectCloudinary from './configs/cloudianry.js'
 
 const app  = express()
 const PORT = process.env.PORT || 3000 ; 
+
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
@@ -12,6 +16,7 @@ app.use(clerkMiddleware())
 
 app.get('/',(req,res)=>res.send('Server is live!'))
 app.use(requireAuth())
+app.use('/api/ai',aiRouter)
 
 app.listen(PORT , ()=>{
     console.log('Server is running on port',PORT)
